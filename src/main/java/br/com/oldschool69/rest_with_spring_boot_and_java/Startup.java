b0ea3a0bb963695ecd0fb5cba536901f771a50c5
Oldschool69 <flavio.marcondes@gmail.com>
2025-06-14 13:14:16 -0300

    Criando versao inicial do projeto

---
+++ src/main/java/br/com/oldschool69/rest_with_spring_boot_and_java/Startup.java
@@ -0,0 +1,13 @@
+package br.com.oldschool69.rest_with_spring_boot_and_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/oldschool69/rest_with_spring_boot_and_java/model/Greeting.java
@@ -0,0 +1,4 @@
+package br.com.oldschool69.rest_with_spring_boot_and_java.model;
+
+public record Greeting(long id, String content) {
+}

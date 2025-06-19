12db46a40427c7adf54665a3348c8999f7228a4b
Oldschool69 <flavio.marcondes@gmail.com>
2025-06-19 11:33:05 -0300

    implementando o Person controller e service

---
+++ src/main/java/br/com/oldschool69/rest_with_spring_boot_and_java/PersonServices.java
@@ -0,0 +1,50 @@
+package br.com.oldschool69.rest_with_spring_boot_and_java;
+
+import br.com.oldschool69.rest_with_spring_boot_and_java.model.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+@Service
+public class PersonServices {
+
+    private final AtomicLong counter = new AtomicLong();
+    private final Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public Person findById(String id){
+
+        logger.info("Finding one person");
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Flavio");
+        person.setLastName("Oliveira");
+        person.setAddress("Campinas - São Paulo - Brazil");
+        person.setGender("male");
+        return person;
+    }
+
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<>();
+
+        for (int i = 1; i < 9; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+
+        return persons;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Firstname " +  i);
+        person.setLastName("Lastname " + i);
+        person.setAddress("Some address in Brazil");
+        person.setGender("male");
+        return person;
+    }
+}
---
+++ src/main/java/br/com/oldschool69/rest_with_spring_boot_and_java/controllers/PersonController.java
@@ -0,0 +1,37 @@
+package br.com.oldschool69.rest_with_spring_boot_and_java.controllers;
+
+import br.com.oldschool69.rest_with_spring_boot_and_java.PersonServices;
+import br.com.oldschool69.rest_with_spring_boot_and_java.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonServices service;
+
+    @RequestMapping(value = "/{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person findById(@PathVariable("id") String id){
+        return service.findById(id);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<Person> findAll() {
+        return service.findAll();
+    }
+}

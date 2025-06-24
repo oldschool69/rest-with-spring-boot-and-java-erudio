d990c43f82383d58714eaed5487c38624a50b390
Flavio Marcondes de Oliveira <fmarcondes@ciandt.com>
2025-06-24 15:15:08 -0300

    documentados outros endpoints usando swagger

--- src/main/java/br/com/oldschool69/rest_with_spring_boot_and_java/controllers/PersonController.java
+++ src/main/java/br/com/oldschool69/rest_with_spring_boot_and_java/controllers/PersonController.java
@@ -1,8 +1,9 @@
 package br.com.oldschool69.rest_with_spring_boot_and_java.controllers;
 
+import br.com.oldschool69.rest_with_spring_boot_and_java.controllers.docs.PersonControllerDocs;
 import br.com.oldschool69.rest_with_spring_boot_and_java.data.dto.v1.PersonDTO;
-import br.com.oldschool69.rest_with_spring_boot_and_java.data.dto.v2.PersonDTOV2;
 import br.com.oldschool69.rest_with_spring_boot_and_java.services.PersonServices;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,13 +11,13 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.List;
 
 
 @RestController
 @RequestMapping("/person")
-public class PersonController {
+@Tag(name = "People", description = "End point to manage People")
+public class PersonController implements PersonControllerDocs {
 
     private final Logger logger = LoggerFactory.getLogger(PersonController.class.getName());
 
@@ -25,54 +26,59 @@ public class PersonController {
 
     @GetMapping(value = "/{id}",
             produces = {
-                MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_JSON_VALUE,
                     MediaType.APPLICATION_XML_VALUE,
                     MediaType.APPLICATION_YAML_VALUE
             }
     )
-    public PersonDTO findById(@PathVariable("id") Long id){
+    @Override
+    public PersonDTO findById(@PathVariable("id") Long id) {
         logger.info("Finding one Person");
         return service.findById(id);
     }
 
     @GetMapping(
-            produces = { MediaType.APPLICATION_JSON_VALUE,
+            produces = {MediaType.APPLICATION_JSON_VALUE,
                     MediaType.APPLICATION_XML_VALUE,
-                    MediaType.APPLICATION_YAML_VALUE }
+                    MediaType.APPLICATION_YAML_VALUE}
     )
+    @Override
     public List<PersonDTO> findAll() {
         logger.info("Finding all person");
         return service.findAll();
     }
 
     @PostMapping(
-            consumes = { MediaType.APPLICATION_JSON_VALUE,
+            consumes = {MediaType.APPLICATION_JSON_VALUE,
                     MediaType.APPLICATION_XML_VALUE,
-                    MediaType.APPLICATION_YAML_VALUE },
-            produces = { MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE,
                     MediaType.APPLICATION_XML_VALUE,
-                    MediaType.APPLICATION_YAML_VALUE }
+                    MediaType.APPLICATION_YAML_VALUE}
     )
+    @Override
     public PersonDTO create(@RequestBody PersonDTO person) {
         logger.info("Creating a new person");
         return service.create(person);
     }
 
     @PutMapping(
-            consumes = { MediaType.APPLICATION_JSON_VALUE,
+            consumes = {MediaType.APPLICATION_JSON_VALUE,
                     MediaType.APPLICATION_XML_VALUE,
-                    MediaType.APPLICATION_YAML_VALUE },
-            produces = { MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE,
                     MediaType.APPLICATION_XML_VALUE,
-                    MediaType.APPLICATION_YAML_VALUE }
+                    MediaType.APPLICATION_YAML_VALUE}
     )
+    @Override
     public PersonDTO update(@RequestBody PersonDTO person) {
         logger.info("Updating a person");
         return service.update(person);
     }
 
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<?> delete(@PathVariable("id") Long id){
+    @Override
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
         logger.info("Deleting a person");
         service.delete(id);
         return ResponseEntity.noContent().build();

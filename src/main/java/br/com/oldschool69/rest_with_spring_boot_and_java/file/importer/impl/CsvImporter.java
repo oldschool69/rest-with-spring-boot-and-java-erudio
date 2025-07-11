bbe497b1f1a6b3264ecfa0f0e1ad234b6552cc8a
Flavio Marcondes de Oliveira <fmarcondes@ciandt.com>
2025-07-11 15:53:07 -0300

    Criando os importadores de csv e xlsx

--- src/main/java/br/com/oldschool69/rest_with_spring_boot_and_java/file/importer/impl/CsvImporter.java
+++ src/main/java/br/com/oldschool69/rest_with_spring_boot_and_java/file/importer/impl/CsvImporter.java
@@ -2,13 +2,46 @@ package br.com.oldschool69.rest_with_spring_boot_and_java.file.importer.impl;
 
 import br.com.oldschool69.rest_with_spring_boot_and_java.data.dto.v1.PersonDTO;
 import br.com.oldschool69.rest_with_spring_boot_and_java.file.importer.contract.FileImporter;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.stereotype.Component;
+
 
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class CsvImporter  implements FileImporter {
     @Override
     public List<PersonDTO> importFile(InputStream inputStream) throws Exception {
-        return List.of();
+        CSVFormat format = CSVFormat.Builder.create()
+                .setHeader()
+                .setSkipHeaderRecord(true)
+                .setIgnoreEmptyLines(true)
+                .setTrim(true)
+                .get();
+
+        Iterable<CSVRecord> records = format.parse(new InputStreamReader(inputStream));
+
+        return parseRecordsToPersonDTOs(records);
+    }
+
+    private List<PersonDTO> parseRecordsToPersonDTOs(Iterable<CSVRecord> records) {
+
+        List<PersonDTO> people = new ArrayList<>();
+
+        for(CSVRecord record: records) {
+            PersonDTO person = new PersonDTO();
+            person.setFirstName(record.get("first_name"));
+            person.setLastName(record.get("last_name"));
+            person.setAddress(record.get("address"));
+            person.setGender(record.get("gender"));
+            person.setEnabled(true);
+            people.add(person);
+        }
+
+        return people;
     }
 }

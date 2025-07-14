ccdca4523c7339a4bddf94a5afe7783d6bdb9eca
Flavio Marcondes de Oliveira <fmarcondes@ciandt.com>
2025-07-14 14:46:55 -0300

    Finalizada logica do exportador de arquivos

---
+++ src/main/java/br/com/oldschool69/rest_with_spring_boot_and_java/file/exporter/factory/FileExporterFactory.java
@@ -0,0 +1,33 @@
+package br.com.oldschool69.rest_with_spring_boot_and_java.file.exporter.factory;
+
+import br.com.oldschool69.rest_with_spring_boot_and_java.exception.BadRequestException;
+import br.com.oldschool69.rest_with_spring_boot_and_java.file.exporter.MediaTypes;
+import br.com.oldschool69.rest_with_spring_boot_and_java.file.exporter.contract.FileExporter;
+import br.com.oldschool69.rest_with_spring_boot_and_java.file.exporter.impl.CsvExporter;
+import br.com.oldschool69.rest_with_spring_boot_and_java.file.exporter.impl.XlsxExporter;
+import br.com.oldschool69.rest_with_spring_boot_and_java.file.importer.contract.FileImporter;
+import br.com.oldschool69.rest_with_spring_boot_and_java.file.importer.impl.CsvImporter;
+import br.com.oldschool69.rest_with_spring_boot_and_java.file.importer.impl.XlsxImporter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileExporterFactory {
+    private Logger logger = LoggerFactory.getLogger(FileExporterFactory.class);
+
+    @Autowired
+    private ApplicationContext context;
+
+    public FileExporter getExporter(String acceptHeader) throws Exception {
+        if (acceptHeader.equalsIgnoreCase(MediaTypes.APPLICATION_XLSX_VALUE)) {
+            return context.getBean(XlsxExporter.class);
+        } else if(acceptHeader.endsWith(MediaTypes.APPLICATION_CSV_VALUE)) {
+            return context.getBean(CsvExporter.class);
+        } else {
+            throw new BadRequestException("Invalid file format");
+        }
+    }
+}

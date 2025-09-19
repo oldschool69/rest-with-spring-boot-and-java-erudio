200eb0f69f71fb072566ce61f777d339534d7968
Oldschool69 <flavio.marcondes@gmail.com>
2025-09-19 12:44:06 -0300

    criando deploy kubernetes V1 - fix

--- src/main/java/br/com/oldschool69/rest_with_spring_boot_and_java/services/InstanceInformationService.java
+++ src/main/java/br/com/oldschool69/rest_with_spring_boot_and_java/services/InstanceInformationService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class InstanceInformationService {
 
-    private static final String HOST_NAME = "HOST_NAME";
+    private static final String HOST_NAME = "HOSTNAME";
 
     private static final String DEFAULT_ENV_INSTANCE_GUID = "LOCAL";
 

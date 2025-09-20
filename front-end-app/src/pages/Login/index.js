c09345347c2f74cd78c6fda5e041192ce1bc2fe2
Oldschool69 <flavio.marcondes@gmail.com>
2025-09-20 12:26:01 -0300

    Formatando tela de login

--- front-end-app/src/pages/Login/index.js
+++ front-end-app/src/pages/Login/index.js
@@ -1,7 +1,21 @@
 import './styles.css'
 
+import logoImage from '../../assets/logo.svg'
+import padlock from '../../assets/padlock.png'
+
 export default function Login() {
     return (
-            <h1>Wellcome</h1>
+        <div className='login-container'>
+            <section className='form'>
+                <img src={logoImage} alt="Carecao Logo"></img>
+                <form>
+                    <h1>Acess your account</h1>
+                    <input placeholder='Username'></input>
+                    <input type='password' placeholder='Password'></input>
+                    <button className='button' type='submit'>Login</button>
+                </form>
+            </section>
+            <img src={padlock} alt="Login"></img>
+        </div>
     );
 }

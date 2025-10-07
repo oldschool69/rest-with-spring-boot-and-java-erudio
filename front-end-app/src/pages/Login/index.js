66ea2c71155fbfc09f40c717a0dc47fded2771c3
Flavio Marcondes de Oliveira <fmarcondes@ciandt.com>
2025-10-07 15:13:40 -0300

    integrando tela de autenticacao a api

--- front-end-app/src/pages/Login/index.js
+++ front-end-app/src/pages/Login/index.js
@@ -1,17 +1,45 @@
+import {useState} from 'react';
+import {useHistory} from 'react-router-dom';
+import api from '../../services/api';
+
 import './styles.css'
+
 
 import logoImage from '../../assets/logo.svg'
 import padlock from '../../assets/padlock.png'
 
 export default function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const history = useHistory();
+
+    async function login(e){
+        e.preventDefault();
+
+        const data = {
+            username,
+            password
+        };
+
+        try{
+            const response = await api.post('/auth/signin', data);
+            localStorage.setItem('username', username);
+            localStorage.setItem('accessToken', response.data.accessToken);
+
+            history.push('/books');
+        }catch(err){
+            alert('Login failed, try again.');
+        }
+    }
+
     return (
         <div className='login-container'>
             <section className='form'>
                 <img src={logoImage} alt="Carecao Logo"></img>
-                <form>
+                <form onSubmit={login}>
                     <h1>Acess your account</h1>
-                    <input placeholder='Username'></input>
-                    <input type='password' placeholder='Password'></input>
+                    <input placeholder='Username' value={username} onChange={e => setUsername(e.target.value)}></input>
+                    <input type='password' placeholder='Password' value={password} onChange={e => setPassword(e.target.value)}></input>
                     <button className='button' type='submit'>Login</button>
                 </form>
             </section>
